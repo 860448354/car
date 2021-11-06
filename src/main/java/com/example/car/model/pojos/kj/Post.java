6920a31ef774c5e38cb650c3bac2a23626b9095f
kg1926 <896967170@qq.com>
2021-11-06 18:25:48 +0800

    员工

---
+++ src/main/java/com/example/car/model/pojos/kj/Post.java
@@ -0,0 +1,22 @@
+package com.example.car.model.pojos.kj;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+  private Integer ptId;
+  private String ptName;
+  private Integer dtId;
+
+
+
+
+
+}
