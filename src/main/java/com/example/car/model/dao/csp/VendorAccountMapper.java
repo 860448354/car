4046a8726a69e6f046cbc90c73e365e4488c384a
Administrator <11111111>
2021-11-07 21:47:00 +0800

    供货商结算

--- src/main/java/com/example/car/model/dao/csp/VendorAccountMapper.java
+++ src/main/java/com/example/car/model/dao/csp/VendorAccountMapper.java
@@ -1,11 +1,23 @@
 package com.example.car.model.dao.csp;
 
+import com.example.car.model.pojos.csp.VendorAccount;
 import com.example.car.model.pojos.hyj.Purchase;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface VendorAccountMapper {
+    //查询未审核结算的采购订单
     public List<Purchase> selPurchase(String supplierName);
+
+    //新增供货商结算单
+    public void addVendorAccount(VendorAccount vendorAccount);
+
+    //修改采购订单状态
+    public void editPurchase(@Param("sfName") String sfName, @Param("purOrder") String purOrder);
+
+    //查询供货商往来账
+    public List<VendorAccount> selVendorAccount(@Param("start") String start,@Param("end")String end,@Param("supplierName")String supplierName);
 }
